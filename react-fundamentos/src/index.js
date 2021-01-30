0e7a599db6d8f75ed68f9b8424b11300bf8987c2
alanmartinc <alancabot.ac@gmail.com>
2021-01-30 19:00:13 -0300

    Se agrega ejercicio expresiones en JSX

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = <h1>Hello React</h1>
-const root = document.getElementById('root')
+const user1 = {
+    name: 'Alan Cabot',
+    age: '24',
+    country: 'Argentina'
+}
 
-ReactDOM.render(App, root)
+function getInfo (user) {
+    return `Hola mi nombre es ${user.name} y soy originario de ${user.country}`
+}
+
+const App = <h1>Hello {user1.name}, tu edad es {user1.age} años y vivis en {user1.country}. <br></br> {getInfo(user1)}</h1>
+
+ReactDOM.render(App, document.getElementById('root'))
