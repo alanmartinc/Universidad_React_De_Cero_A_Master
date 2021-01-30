37654ae0d7a4fd0b4261d0db125c8ee0c7acb082
alanmartinc <alancabot.ac@gmail.com>
2021-01-30 19:26:07 -0300

    Se agrega ejercicio que son las props en un componente de React

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const TarjetaFruta = () => (
-    <div>
-        <h3>Titulo</h3>
-        <hr/>
-        <p>Descripcion...</p>
-    </div>
-)
+const TarjetaFruta = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h3>{props.name}</h3>
+            <hr/>
+            <p>$ {props.price}</p>
+        </div>
+    )
+}
 
 const App = () => (
     <div>
-        <TarjetaFruta/>
+        <TarjetaFruta name='Sandia' price={4.00}/>
+        <TarjetaFruta name='Naranja' price={2.00}/>
+        <TarjetaFruta name='Kiwi' price={6.00}/>
     </div>
 )
 
