e5a9830c84afe500cbba992ae6576262236dd05b
alanmartinc <alancabot.ac@gmail.com>
2021-01-30 19:08:14 -0300

    Se agrega ejercicio crear componentes funcional con React

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const user1 = {
-    name: 'Alan Cabot',
-    age: '24',
-    country: 'Argentina'
-}
+const TarjetaFruta = () => (
+    <div>
+        <h3>Titulo</h3>
+        <hr/>
+        <p>Descripcion...</p>
+    </div>
+)
 
-function getInfo (user) {
-    return `Hola mi nombre es ${user.name} y soy originario de ${user.country}`
-}
+const App = () => (
+    <div>
+        <TarjetaFruta/>
+    </div>
+)
 
-const App = <h1>Hello {user1.name}, tu edad es {user1.age} años y vivis en {user1.country}. <br></br> {getInfo(user1)}</h1>
-
-ReactDOM.render(App, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
