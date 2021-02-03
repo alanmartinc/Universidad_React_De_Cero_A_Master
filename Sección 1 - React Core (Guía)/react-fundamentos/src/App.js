6ef19c368ec7f9b47b45ec61d1f6d23b171ac671
alanmartinc <alancabot.ac@gmail.com>
2021-02-03 20:09:33 -0300

    Se agrega ejercicio entendiendo el Object.assign (ES6)

--- Sección 1 - React Core (Guía)/react-fundamentos/src/App.js
+++ Sección 1 - React Core (Guía)/react-fundamentos/src/App.js
@@ -9,4 +9,39 @@ const App = () => (
     </div>
 )
 
+const perfil = {
+    nombre: 'Gerardo',
+    info: {
+        direccion: 'La direccion...'
+    }
+}
+
+const region = {
+    pais: 'Mexico',
+    // Este objeto sustituye al info de arriba
+    info: {
+        coordenadas: 'Coordenadas...'
+    }
+}
+
+const social = {
+    twitter: '@luxfenix'
+}
+
+const resultado = Object.assign(
+    {},
+    perfil,
+    region,
+    social,
+    {
+        info: Object.assign(
+            {},
+            perfil.info,
+            region.info
+        )
+    }
+)
+
+console.log(resultado);
+
 export default App
