e26e50b253d105df45bd2c1197e197d2b784bc56
alanmartinc <alancabot.ac@gmail.com>
2021-02-03 10:15:09 -0300

    Se agrega ejercicio aplicar estilos CSS en linea a componente de React

--- Sección 1 - React Core (Guía)/react-fundamentos/src/componentes/TarjetaFruta.js
+++ Sección 1 - React Core (Guía)/react-fundamentos/src/componentes/TarjetaFruta.js
@@ -24,8 +24,22 @@ class TarjetaFruta extends React.Component {
     }
 
     render () {
+        // Constante para lógica CSS
+        const hasItems = this.state.cantidad > 0
+
+        // Forma de aplicar estilos
+        const styles = {
+            border:'1px solid black',
+            marginBottom: '1em',
+            borderRadius: '0.5em',
+            padding: '1em',
+            background: hasItems ? 'linear-gradient(45deg, black, #4a02f7)' : '#FFF',
+            color: hasItems > 0 ? '#FFF' : '#000',
+            transition: 'all 400ms ease-out'
+        }
+
         return (
-            <div>
+            <div style={styles}>
                 <h3>{this.props.name}</h3>
                 <hr/>
                 <div>Cantidad: {this.state.cantidad}</div>
@@ -39,6 +53,9 @@ class TarjetaFruta extends React.Component {
                     onClick={this.limpiar}>Limpiar
                 </button>
                 <p>$ {this.props.price}</p>
+                <p>
+                    Total: $ {this.props.price * this.state.cantidad}
+                </p>
             </div>
         )
     }
