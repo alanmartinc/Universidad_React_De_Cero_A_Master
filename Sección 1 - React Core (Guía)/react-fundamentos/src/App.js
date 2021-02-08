f7828151033eda6d2aed6348a5a9a36df137c24b
alanmartinc <alancabot.ac@gmail.com>
2021-02-08 09:13:12 -0300

    Se agrega ejercicio crear eventos personalizados con React

--- Sección 1 - React Core (Guía)/react-fundamentos/src/App.js
+++ Sección 1 - React Core (Guía)/react-fundamentos/src/App.js
@@ -1,34 +1,40 @@
 import React, {Component} from 'react';
+import './global.css'
 
-// Conservar eventos con React
-class PersistenciaEventos extends Component {
-    state = {
-        color: 'blue'
+// Crear eventos personalizados con React
+class Hijo extends Component {
+    manejadorClick = () => {
+        this.props.onSaluda('Ninja en React')
     }
-    handlerChange = (event) => {
-        const color = event.target.value
-
-        this.setState(state => ({
-            color
-        }))
-    }
-
     render() {
         return(
-            <div>
-                <input type="text" onChange={this.handlerChange}></input>
-                <h1 style={{color: this.state.color}}>
-                    {this.state.color}
-                </h1>
+            <div className='box blue'>
+                <h2>Hijo</h2>
+                <button onClick={this.manejadorClick}>
+                    Saluda
+                </button>
             </div>
         )
     }
 }
 
-const App = () => (
-    <div>
-        <PersistenciaEventos/>
-    </div>
-)
+class App extends Component {
+    state = {
+        name: ''
+    }
+    manejador = (name) => {
+        this.setState({name})
+    }
+    render() {
+        return(
+            <div className='box red'>
+                <Hijo onSaluda={this.manejador}/>
+                <h1>
+                    Nombre: {this.state.name}
+                </h1>
+            </div>
+        )
+    }
+}
 
 export default App
