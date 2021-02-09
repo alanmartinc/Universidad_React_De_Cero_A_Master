d0f3f64c5aaa52471881970f81f119a6e04af244
alanmartinc <alancabot.ac@gmail.com>
2021-02-09 10:31:59 -0300

    Se agrega ejercicio iterando listas con React

--- Sección 2 - Iterando listas en React/react-fundamentos/src/App.js
+++ Sección 2 - Iterando listas en React/react-fundamentos/src/App.js
@@ -1,53 +1,24 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
 
-const noop = () => {}
-
-// Controlar los datos de entrada con prop-types
-class Profile extends Component {
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        bio: PropTypes.string,
-        email: PropTypes.string,
-        age: PropTypes.number
-    }
-
-    // Datos de entrada por defecto con defaultProps
-    static defaultProps = {
-        name: 'Ninja PRO',
-        onHello: noop
-    }
-
-    saluda = () => {
-        this.props.onHello()
-    }
-
-    render() {
-        const { name, bio, email} = this.props
-
-        return (
-            <div>
-                <h1>{name}</h1>
-                <p>{bio}</p>
-                <a href={`mailto:${email}`}>
-                    {email}
-                </a>
-                <button onClick={this.saluda}>
-                    Saluda
-                </button>
-            </div>
-        )
-    }
-}
+// Iterando listas con React
+const frutas = [
+    'frutilla',
+    'manzana',
+    'naranja',
+    'kiwi'
+]
 
 class App extends Component {
     render() {
         return(
             <div>
-                <Profile
-                    bio='Soy un desarrollador Frontend'
-                    email='my-email@mail.com'
-                />
+                <ul>
+                    {frutas.map((fruta) => {
+                        return (
+                            <li>{fruta}</li>
+                        )
+                    })}
+                </ul>
             </div>
         )
     }
