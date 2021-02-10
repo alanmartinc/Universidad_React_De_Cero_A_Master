915ce47916471a2e21e4155edc7b8f2b4df470b1
alanmartinc <alancabot.ac@gmail.com>
2021-02-10 02:25:43 -0300

    Se agrega ejercicio que son las refs en React

--- Sección 3 - Entendiendo las Refs en React/react-fundamentos/src/App.js
+++ Sección 3 - Entendiendo las Refs en React/react-fundamentos/src/App.js
@@ -1,48 +1,48 @@
 import React, { Component } from 'react'
 
-// Pasar datos al iterar listas con React
+// Que son las Refs en React
+class Entrada extends Component {
+  constructor() {
+    super()
+
+    this.entrada = React.createRef()
+  }
+
+  focus = () => {
+    this.entrada.current.focus()
+  }
+
+  // Pone el focus al cargar
+  componentDidMount() {
+    this.focus()
+  }
+
+  blur = () => {
+    this.entrada.current.blur()
+  }
+
+  render() {
+    return(
+      <div>
+        <input type="text" ref={this.entrada}></input>
+        <button onClick={this.focus}>
+          Focus
+        </button>
+        <button onClick={this.blur}>
+          Blur
+        </button>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
-  state = {
-    fruits: [
-      { name: 'Fresa', price: 5.4 },
-      { name: 'Manzana', price: 2.3 },
-      { name: 'Sandia', price: 1.2 },
-      { name: 'Guayaba', price: 5 },
-      { name: 'Pera', price: 4.12 },
-      { name: 'Kiwi', price: 3.87 },
-      { name: 'Limon', price: 7.21 },
-      { name: 'Melon', price: 6.21 },
-      { name: 'Cereza', price: 4.98 }
-    ],
-
-    frutaSeleccionada: {}
-  }
-
-  select = (frutaSeleccionada, event) => {
-    this.setState({
-      frutaSeleccionada
-    })
-  }
-
   render () {
-    const { fruits, frutaSeleccionada } = this.state
-
     return (
-      <ul>
-        {fruits.map(fruit => (
-          <li
-            key={fruit.name}
-            onClick={this.select.bind(this, fruit)}
-            style={{
-              color: frutaSeleccionada.name === fruit.name
-                ? 'red'
-                : '#000'
-            }}
-          >
-            { fruit.name } - $ { fruit.price }
-          </li>
-        ))}
-      </ul>
+      <div>
+        <h1>React refs</h1>
+        <Entrada/>
+      </div>
     )
   }
 }
