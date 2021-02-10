d1b7109c4f6fe5384cd31cea3708a8c415a3abf7
alanmartinc <alancabot.ac@gmail.com>
2021-02-10 02:44:39 -0300

    Se agrega ejercicio integrar librerias de terceros usando las Refs de React

--- Sección 3 - Entendiendo las Refs en React/react-fundamentos/src/App.js
+++ Sección 3 - Entendiendo las Refs en React/react-fundamentos/src/App.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react'
+import Chart from 'chart.js'
 
-// Que son las Refs en React
-class Entrada extends Component {
-  constructor() {
-    super()
+// Integrar librerías de terceros usando las Refs de React
+class Graficas extends Component {
+  // Creo la Refs
+  grafica = React.createRef()
 
-    this.entrada = React.createRef()
-  }
-
-  focus = () => {
-    this.entrada.current.focus()
-  }
-
-  // Pone el focus al cargar
+  // Se ejecuta cuando se crea el marcado inicial
   componentDidMount() {
-    this.focus()
-  }
+    const ctx = this.grafica.current.getContext('2d');
 
-  blur = () => {
-    this.entrada.current.blur()
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  })
   }
 
   render() {
     return(
       <div>
-        <input type="text" ref={this.entrada}></input>
-        <button onClick={this.focus}>
-          Focus
-        </button>
-        <button onClick={this.blur}>
-          Blur
-        </button>
+        <canvas ref={this.grafica} width="400" height="400"></canvas>
       </div>
     )
   }
@@ -40,8 +61,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <h1>React refs</h1>
-        <Entrada/>
+        <Graficas/>
       </div>
     )
   }
