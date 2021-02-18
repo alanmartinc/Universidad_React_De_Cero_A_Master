011ebcdaa1d13144e52dffccc6f27e583f31989a
alanmartinc <alancabot.ac@gmail.com>
2021-02-18 14:27:08 -0300

    Se agrega ejercicio solución a tarea (buscador de películas)

--- Sección 6 - Solicitudes HTTP en React/react-fundamentos/src/App.js
+++ Sección 6 - Solicitudes HTTP en React/react-fundamentos/src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
 
-// Ejemplo buscador de peliculas
+// Solución a Tarea (buscador de películas)
 class App extends Component {
   state = {
-    movie: {}
+    movie: {},
+    isFetching: false
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
+
+    this.setState({isFetching: true})
+
     const title = event.target[0].value
     const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=5c24385e'
     fetch(url + '&t=' + title)
       .then(res => res.json())
-      .then(movie => this.setState({movie}))
+      .then(movie => this.setState({movie, isFetching: false}))
   }
 
   render() {
-    const {movie} = this.state
+    const {movie, isFetching} = this.state
 
     return(
       <div>
@@ -26,15 +30,22 @@ class App extends Component {
             Buscar
           </button>
         </form>
-        <h1>{movie.Title}</h1>
-        <p>{movie.Plot}</p>
-        <img 
-          src={movie.Poster} 
-          alt="Poster"
-          style={{
-            width: '150px'
-          }}>
-        </img>
+        {isFetching && (
+          <h2>Cargando...</h2>
+        )}
+        {movie.Title && (
+          <div>
+            <h1>{movie.Title}</h1>
+            <p>{movie.Plot}</p>
+            <img 
+              src={movie.Poster} 
+              alt="Poster"
+              style={{
+                width: '150px'
+              }}>
+            </img>
+          </div>
+        )}
       </div>
     )
   }
