6d81f42c41b3eefc468c1df3660087c102e6364e
alanmartinc <alancabot.ac@gmail.com>
2021-02-18 23:48:51 -0300

    Se agrega ejercicio el metodo componentDidMount

--- Sección 7 - Master en Ciclo de vida de componente en React/react-fundamentos/src/App.js
+++ Sección 7 - Master en Ciclo de vida de componente en React/react-fundamentos/src/App.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react'
 
-// El metodo render
-const getList = () => {
-  return(
-    <ul>
-      <li>Fresa</li>
-      <li>Sandia</li>
-      <li>Mango</li>
-    </ul>
-  )
+// El metodo componentDidMount
+class Http extends Component {
+  state = {
+    photos: []
+  }
+
+  componentDidMount() {
+    // Solicitudes HTTP
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(res => res.json())
+      .then(photos => this.setState({photos}))
+  }
+
+  render() {
+    const {photos} = this.state
+
+    return(
+      <div>
+        {photos.map(photo => (
+          <img 
+            key={photo.id}
+            src={photo.thumbnaiUrl}
+            alt={photo.title}
+          />
+        ))}
+      </div>
+    )
+  }
+}
+
+class Events extends Component {
+  state = {
+    width: window.innerWidth
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+  }
+  
+  handleResize = () => {
+    this.setState({width: window.innerWidth})
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Events {this.state.width}</h2>
+      </div>
+    )
+  }
 }
 
 class App extends Component {
   render() {
     return(
       <div>
-        <h1>Metodo render</h1>
-        {getList()}
+        <h1>Metodo componentDidMount</h1>
+        <Http/>
+        <Events/>
       </div>
     )
   }
