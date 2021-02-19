4f5190ff01eb5fc297e5d31aff0ecc9e9b511055
alanmartinc <alancabot.ac@gmail.com>
2021-02-18 23:04:11 -0300

    Se agrega ejercicio el metodo render

--- Sección 7 - Master en Ciclo de vida de componente en React/react-fundamentos/src/App.js
+++ Sección 7 - Master en Ciclo de vida de componente en React/react-fundamentos/src/App.js
@@ -1,45 +1,22 @@
 import React, { Component } from 'react'
 
-// El metodo constructor
-class Contador extends Component {
-  constructor(props) {
-    super(props)
-
-    this.agregar = this.agregar.bind(this)
-
-    this.title = React.createRef()
-
-    this.state = {
-      num: this.props.num
-    }
-  }
-
-  agregar() {
-    console.log(this.title)
-
-    this.setState(state => ({
-      num: state.num + 1
-    }))
-  }
-
-  render() {
-    return(
-      <div>
-        <h2 ref={this.title}>{this.state.message}</h2>
-        <button onClick={this.agregar}>
-          Click({this.state.num})
-        </button>
-      </div>
-    )
-  }
+// El metodo render
+const getList = () => {
+  return(
+    <ul>
+      <li>Fresa</li>
+      <li>Sandia</li>
+      <li>Mango</li>
+    </ul>
+  )
 }
 
 class App extends Component {
   render() {
     return(
       <div>
-        <h1>Metodo constructor</h1>
-        <Contador num={1200}/>
+        <h1>Metodo render</h1>
+        {getList()}
       </div>
     )
   }
