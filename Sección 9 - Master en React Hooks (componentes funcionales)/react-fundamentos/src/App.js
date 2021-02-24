d71458897165a4076e32a7bafcac26c1460f4d0b
alanmartinc <alancabot.ac@gmail.com>
2021-02-24 09:45:43 -0300

    Se agrega ejercicios Hook useState

--- Sección 9 - Master en React Hooks (componentes funcionales)/react-fundamentos/src/App.js
+++ Sección 9 - Master en React Hooks (componentes funcionales)/react-fundamentos/src/App.js
@@ -1,13 +1,44 @@
-import React, {Component} from 'react'
+import React, {Component, useState} from 'react'
 
-class App extends Component {
-    render() {
-        return(
-            <div>
-
-            </div>
-        )
+// Hook useState
+const Header = () => {
+    const subtitleStyles = {
+        fontWeight: 'bold'
     }
+
+    const headerStyles = {
+        margin: '0.3em',
+        padding: '.3em',
+        color: '#FFF',
+        borderRadius: '0.2em',
+        textAlign: 'center',
+        background: 'linear-gradient(20deg, #6813cb, #2575fc)',
+        fontSize: '14px'
+    }
+
+    return(
+        <header style={headerStyles}>
+            <h1>
+                Hook useState
+                <span role='img' aria-label='hook emoji'>⚓</span>
+            </h1>
+        </header> 
+    )
+}
+
+const App = () => {
+    const [clicks, setClicks] = useState(0)
+
+    return(
+        <div>
+            <Header/>
+            <button onClick={() => {
+                setClicks(clicks + 1)
+            }}>
+                Clicks ({clicks})
+            </button>
+        </div>
+    )
 }
 
 export default App
