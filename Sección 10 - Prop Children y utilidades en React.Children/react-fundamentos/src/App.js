6de8f2cfd0fcf3c7aba7598aab2fe24a9e2864d1
alanmartinc <alancabot.ac@gmail.com>
2021-03-02 15:18:41 -0300

    Se agrega ejercicios entendiendo la Prop Children en profundidad

--- Sección 10 - Prop Children y utilidades en React.Children/react-fundamentos/src/App.js
+++ Sección 10 - Prop Children y utilidades en React.Children/react-fundamentos/src/App.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react'
+import './global.css'
 
-const App = () => {
-    return (
-    <div>
-        
-    </div>
-    )
+// Entendiendo la Prop Children en profundidad
+class Parent extends Component {
+    render() {
+        const {children} = this.props
+
+        return(
+            <div className='box'>
+                <div className='box blue'>
+    
+                </div>
+    
+                <div className='box red'>
+                    {children}
+                </div>
+            </div>
+        )
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Parent>
+                    Hijo de Texto
+                </Parent>
+            </div>
+        )
+    }
 }
 
 export default App
