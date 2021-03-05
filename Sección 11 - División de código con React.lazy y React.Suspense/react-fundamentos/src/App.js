964e28528f8e5a9330f0d961d674c2567daf45ce
alanmartinc <alancabot.ac@gmail.com>
2021-03-05 12:22:07 -0300

    Se agrega ejercicios ejemplo React.lazy y React.Suspense

--- Sección 11 - División de código con React.lazy y React.Suspense/react-fundamentos/src/App.js
+++ Sección 11 - División de código con React.lazy y React.Suspense/react-fundamentos/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState, lazy, Suspense} from 'react'
+import {MoonLoader} from 'react-spinners'
+
+// Code Splitting - Importación Dinámica
+const Image = lazy(() => import('./components/Image'))
 
 // Ejemplo React.lazy y React.Suspense
 const App = () => {
-    return(
-        <div>
+    const [show, setShow] = useState(false)
 
+    const toggle = () => setShow(!show)
+
+    const styles = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    }
+
+    return(
+        <div style={styles}>
+            <button onClick={toggle}>
+                {show ? 'Ocultar' : 'Mostrar'}
+            </button>
+            {show && (
+                <Suspense fallback={<MoonLoader color='orangered'/>}>
+                    <Image/>
+                </Suspense>
+            )}
         </div>
     )
 }
---
+++ Sección 11 - División de código con React.lazy y React.Suspense/react-fundamentos/src/components/Image.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Image.css'
+
+const Image = () => {
+  return (
+    <div className='show'>
+      <img
+        src='https://images.pexels.com/photos/3048873/pexels-photo-3048873.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        alt='Imagen increible'
+        width='100%'
+      />
+    </div>
+  )
+}
+
+export default Image
