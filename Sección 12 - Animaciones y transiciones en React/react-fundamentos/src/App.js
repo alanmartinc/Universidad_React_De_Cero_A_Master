7c705d2c4071044b1363de7464aeef4edc7de3b1
alanmartinc <alancabot.ac@gmail.com>
2021-03-07 16:03:47 -0300

    Se agrega ejercicios transiciones con estilos en linea CSS

--- Sección 12 - Animaciones y transiciones en React/react-fundamentos/src/App.js
+++ Sección 12 - Animaciones y transiciones en React/react-fundamentos/src/App.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-// Introducción a curso animaciones y transiciones
+// Transiciones con estilos en linea CSS
+const Header = ({show}) => {
+    const activeStyles = {
+        background: '#3d1f9a',
+        transform: 'scale(1)'
+    }
+
+    let headerStyles = {
+        background: 'black',
+        transform: 'scale(0)',
+        position: 'absolute',
+        textAlign: 'center',
+        borderRadius: '.4em',
+        color: '#FFF',
+        padding: '0.5em',
+        margin: '0.5em',
+        fontSize: '14px',
+        transition: 'all 800ms ease'
+    }
+
+    if(show) {
+        headerStyles = {
+            ...headerStyles,
+            ...activeStyles
+        }
+    }
+
+    return(
+        <header style={headerStyles}>
+            <h1>
+                Transiciones CSS en linea
+                <span role='img' aria-label='fire'>
+                    🔥
+                </span>
+            </h1>
+        </header>
+    )
+}
+
 const App = () => {
+    const[active, setActive] = useState(false)
+
+    const toggle = () => setActive(!active)
+
     return(
         <div>
-
+            <button onClick={toggle}>
+                {active ? 'Desactivar' : 'Activar'}
+            </button>
+            <Header show={active}/>
         </div>
     )
 }
