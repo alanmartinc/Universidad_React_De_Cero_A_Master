112335b8371ef6d79eb37cb505cd9be62d23e0bf
alanmartinc <alancabot.ac@gmail.com>
2021-03-09 12:14:06 -0300

    Se agrega ejercicios instalación de styled-components y sintaxis

--- Sección 13 - Gestión de Estilos CSS Avanzados con styled-components/react-fundamentos/src/App.js
+++ Sección 13 - Gestión de Estilos CSS Avanzados con styled-components/react-fundamentos/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+// Instalación de styled-components y sintaxis
+const Header = styled.header `
+  background: linear-gradient(20deg, #db7093, #daa357);
+  text-align: center;
+  border-radius: 0.2em;
+  color: #FFF;
+  padding: 0.3em;
+  margin: 0.3em;
+  font-size: 14px;
+`
 
 const App = () => {
   return (
     <div>
-
+      <Header>
+        <h1>Styled Components</h1>
+      </Header>
     </div>
   )
 }
