fa11bed32cdbafc44bfd1b49b3a6af0bb595a32f
alanmartinc <alancabot.ac@gmail.com>
2021-03-11 12:02:40 -0300

    Se agrega ejercicios pseudo clases CSS

--- Sección 13 - Gestión de Estilos CSS Avanzados con styled-components/react-fundamentos/src/App.js
+++ Sección 13 - Gestión de Estilos CSS Avanzados con styled-components/react-fundamentos/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// Estilizando con clases CSS en elementos anidados
+// Pseudo Clases CSS
 const Header = styled.header `
   background: linear-gradient(20deg, #db7093, #daa354);
   text-align: center;
@@ -10,14 +10,19 @@ const Header = styled.header `
   padding: 0.3em;
   margin: 0.3em;
   font-size: 14px;
+  transition: opacity 350ms ease-out;
+  opacity: 0.5;
 
-  h1 {
-    color: white;
+  &:hover {
+    opacity: 1;
+
+    h1 {
+      color: red;
+    }
   }
 
-  .big {
-    font-size: 20px;
-    color: black;
+  h1 {
+    color: purple;
   }
 `
 
@@ -28,9 +33,6 @@ const App = () => {
         <h1>
           Styled Components
         </h1>
-        <div className='big'>
-          Ejemplo
-        </div>
       </Header>
     </div>
   )
