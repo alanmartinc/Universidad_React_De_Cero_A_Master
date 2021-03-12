c363d83df4b6097b32434efa51f6081792c25c00
alanmartinc <alancabot.ac@gmail.com>
2021-03-12 10:38:26 -0300

    Se agrega ejercicios Props disponibles en componente Route

--- Sección 14 - React Router V5/react-fundamentos/src/App.js
+++ Sección 14 - React Router V5/react-fundamentos/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 
-// Instalación de React Router y montando primeras rutas
+// Props disponibles en componente Route
 const Hola = () => (
   <h1>Hola</h1>
 )
@@ -10,19 +10,17 @@ const Productos = () => (
   <h1>Productos</h1>
 )
 
+const Home = () => (
+  <h1>Home</h1>
+)
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path='/hola' component={Hola}/>
-      
-      <section>
-        <Route path='/productos' component={Productos}/>
-      </section>
-      
-      <div>
-        <Route path='/productos' component={Productos}/>
-      </div>
+        <Route path='/' exact component={Home}/>
+        <Route path='/hola/' strict component={Hola}/>
+        <Route path='/productos' sensitive component={Productos}/>
       </BrowserRouter>
     </div>
   )
