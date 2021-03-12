f3e2f9df0d75187b6b8b43a5997b9295f30a783c
alanmartinc <alancabot.ac@gmail.com>
2021-03-12 10:01:40 -0300

    Se agrega ejercicios instalación de React Router y montando primeras rutas

--- Sección 14 - React Router V5/react-fundamentos/src/App.js
+++ Sección 14 - React Router V5/react-fundamentos/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+// Instalación de React Router y montando primeras rutas
+const Hola = () => (
+  <h1>Hola</h1>
+)
+
+const Productos = () => (
+  <h1>Productos</h1>
+)
 
 const App = () => {
   return (
     <div>
-      <h1>
-        React Router
-      </h1>
+      <BrowserRouter>
+        <Route path='/hola' component={Hola}/>
+      
+      <section>
+        <Route path='/productos' component={Productos}/>
+      </section>
+      
+      <div>
+        <Route path='/productos' component={Productos}/>
+      </div>
+      </BrowserRouter>
     </div>
   )
 }
