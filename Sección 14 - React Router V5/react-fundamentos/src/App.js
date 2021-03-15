d5ee5ce77a9b036ce2a24ced41574ab2d28855b9
alanmartinc <alancabot.ac@gmail.com>
2021-03-15 10:03:00 -0300

    Se agrega ejercicios como usar el componente Link

--- Sección 14 - React Router V5/react-fundamentos/src/App.js
+++ Sección 14 - React Router V5/react-fundamentos/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-// Diferentes formas de renderizar con Route
+// Como usar el componente link
 const Hola = () => (
   <h1>Hola</h1>
 )
@@ -10,31 +10,42 @@ const Productos = () => (
   <h1>Productos</h1>
 )
 
-const Home = () => (
-  <h1>Home</h1>
+const Home = (props) => {
+  console.log(props)
+  return(
+    <h1>Home</h1>
+  )
+}
+
+const navStyles = {
+  display: 'flex',
+  justifyContent: 'space-around'
+}
+
+const Navegation = () => (
+  <nav style={navStyles}>
+    <Link to={{
+      pathname: '/',
+      search: '?ordenar=nombre',
+      hash: '#hash-otro',
+      state: {
+        name: 'Ninja PRO',
+        age: 25
+      }
+    }}>Home </Link>
+    <Link to='/hola'>Hola </Link>
+    <Link to='/productos' replace>Productos </Link>
+  </nav>
 )
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path='/' exact component={Home}/>
-
-        <Route path='/hola/' strict render={Hola}/>
-
-        <Route path='/productos'>
-          {({match}) => {
-            if(!match) return(
-              <div>
-                *** Wops no coincide con /productos
-              </div>
-            )
-
-            return(
-              <Productos/>
-            )
-          }}
-        </Route>
+        <Navegation/>
+            <Route path='/' exact render={Home}/>
+            <Route path='/hola/' render={Hola}/>
+            <Route path='/productos' render={Productos}/>
       </BrowserRouter>
     </div>
   )
