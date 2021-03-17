4b3b7497b0d63cce48bf6982f04a799a27b625b5
alanmartinc <alancabot.ac@gmail.com>
2021-03-17 14:25:09 -0300

    Se agrega ejercicios usando el componente Switch

--- Sección 14 - React Router V5/react-fundamentos/src/App.js
+++ Sección 14 - React Router V5/react-fundamentos/src/App.js
@@ -1,95 +1,28 @@
 import React from 'react'
-import {BrowserRouter, Route, Link, NavLink} from 'react-router-dom'
-import queryString from 'query-string'
-import './App.css'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
-// Pasar información con parámetros de consulta
-const Hola = () => (
-  <h1>Hola</h1>
+// Usando el componente Switch
+const Home = () => (
+  <h1>Home</h1>
 )
 
-const Productos = (props) => (
-  <div>
-    <h1>Productos</h1>
-    <Link to='/productos/gamers'>Gamers</Link>
-    <Link to='/productos/hogar'>Hogar</Link>
-  </div>
+const Videos = () => (
+  <h1>Videos</h1>
 )
 
-const ProductosCategoria = ({match}) => {
-  console.log(match)
-  return(
-    <div>
-      <h1>Categoria: {match.params.categoria}</h1>
-    </div>
-  )
-}
-
-const Home = (props) => {
-  console.log(props)
-  return(
-    <h1>Home</h1>
-  )
-}
-
-const navStyles = {
-  display: 'flex',
-  justifyContent: 'space-around'
-}
-
-const NavActive = {
-  color: 'green' 
-}
-
-const Navegation = () => (
-  <nav style={navStyles}>
-    <NavLink to='/' exact activeStyle={NavActive}>Home </NavLink>
-    <NavLink to='/hola' activeClassName='navActive'>Hola </NavLink>
-    <NavLink to='/productos' activeStyle={NavActive} isActive={(match, location) => {
-      console.log(match)
-        if(!match) return false
-        return !match.isExact
-    }}>Productos </NavLink>
-    <NavLink to='/ropa' activeStyle={NavActive}>Ropa</NavLink>
-  </nav>
+const Playlist = () => (
+  <h1>Playlist</h1>
 )
-
-const Ropa = ({location}) => {
-  console.log(location)
-
-  const {color, talla} = new queryString.parse(location.search)
-
-  /*
-  const query = new URLSearchParams(location.search)
-  const color = query.get('color')
-  const talla = query.get('talla')
-  */
-
-  return(
-    <div>
-      <h1>Ropa</h1>
-      <div>
-        Color: {color}
-      </div>
-      <div>
-        Talla: {talla}
-      </div>
-    </div>
-  )
-}
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Navegation/>
-            <Route path='/' exact render={Home}/>
-            <Route path='/hola/' render={Hola}/>
-            <Route path='/productos' exact render={Productos}/>
-            <Route path='/productos/:categoria/:id?' render={ProductosCategoria}/>
-            <Route path='/ropa' render={Ropa}/>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact render={Home}/>
+        <Route path='/videos' render={Videos}/>
+        <Route path='/videos' render={Playlist}/>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
