81803b3570c9cad3b8974229c797585c7b99c4ce
alanmartinc <alancabot.ac@gmail.com>
2021-03-31 13:24:39 -0300

    Se agrega sección 16 - Introducción al Testing (Test Unitarios)

---
+++ Sección 16 - Introducción al Testing (Test Unitarios)/react-fundamentos/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Correr pruebas con Jest
+const App = () => {
+  return (
+    <div>
+      <h1>Introducción a Unit Testing</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ Sección 16 - Introducción al Testing (Test Unitarios)/react-fundamentos/src/utils.js
@@ -0,0 +1,6 @@
+export const getId = () => {
+  return Math
+    .random()
+    .toString(16)
+    .substr(2)
+}
