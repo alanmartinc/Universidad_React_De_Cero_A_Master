e0d7bd968fd2d184feabb356422241162825c7df
alanmartinc <alancabot.ac@gmail.com>
2021-03-31 13:26:33 -0300

    Se agrega ejercicios correr pruebas con Jest

---
+++ Sección 16 - Introducción al Testing (Test Unitarios)/react-fundamentos/src/_test_/App.test.js
@@ -0,0 +1,11 @@
+import App from '../App'
+
+describe('Grupo de pruebas de ejemplo', () => {
+    test('Una prueba unitaria de algo', () => {
+        expect(10).toBe(10)
+    })
+
+    test('Segunda prueba de numeros', () => {
+        expect(10).toBe(10)
+    })
+})
